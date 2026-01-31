0877965205e5b370f503dff1e6f7047e074c4a6d
2rumaki <0821shuuhei@gmail.com>
2026-02-01 01:23:52 +0900

    fix(test): use globalThis instead of global to fix build error
    CIビルドでtscが 'Cannot find name global' エラーになる問題を修正

--- frontend/src/utils/storage.test.ts
+++ frontend/src/utils/storage.test.ts
@@ -19,7 +19,7 @@ const localStorageMock = (() => {
 	};
 })();
 
-Object.defineProperty(global, "localStorage", {
+Object.defineProperty(globalThis, "localStorage", {
 	value: localStorageMock,
 });
 
